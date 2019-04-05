9d18391681936bc15a5e149ee560b360a9122e68
Md Shariful Alam <shariful.alam.shourov@gmail.com>
2019-04-06 00:26:21 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,28 @@
+# Diabetes prediction application using Python
+
+This application is made using sklearn Machine Learning libraries and pyqt5 for its graphical UI. 
+
+Support Vector Machine classifies input data with 80% accuracy. PIMA Indians diabetes dataset from the UCI archive is used to train the model.
+
+<img src="./screenshots/Screenshot.png" width="250">
+
+## How to start
+
+Firstly install all dependencies via the following commands.
+
+```bash
+pip3 install -r requirements.txt
+```
+
+Now train the application in machine as
+
+```bash
+python3 diabetes.py
+```
+
+Finally run the application:
+
+```bash
+python3 gui.py
+```
+Now insert all the requires details to see the result.
---
+++ diabetes.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+#Diabetes Prediction Using Support Vector Machine
+import pickle
+import os
+import sys
+import pandas as pd
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+
+#For training the model
+def train():
+    dataset = pd.read_csv('pima.csv')
+    X = dataset[['F','D','E','B','C']]
+    Y = dataset[['I']]
+    
+    #train test split
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state = 101)
+    
+    from sklearn.svm import SVC
+    model = SVC(kernel='linear')
+    svc=model.fit(X_train,Y_train)
+    
+    #Save Model As Pickle File
+    with open('svc.pkl','wb') as m:
+        pickle.dump(svc,m)
+    test(X_test,Y_test)
+
+#Test accuracy of the model
+def test(X_test,Y_test):
+    with open('svc.pkl','rb') as mod:
+        p=pickle.load(mod)
+    
+    pre=p.predict(X_test)
+    print (accuracy_score(Y_test,pre)) #Prints the accuracy of the model
+
+
+def find_data_file(filename):
+    if getattr(sys, "frozen", False):
+        # The application is frozen.
+        datadir = os.path.dirname(sys.executable)
+    else:
+        # The application is not frozen.
+        datadir = os.path.dirname(__file__)
+
+    return os.path.join(datadir, filename)
+
+
+def check_input(data) ->int :
+    df=pd.DataFrame(data=data,index=[0])
+    with open(find_data_file('svc.pkl'),'rb') as model:
+        p=pickle.load(model)
+    op=p.predict(df)
+    return op[0]
+if __name__=='__main__':
+    train()    
